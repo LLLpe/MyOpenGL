f1901f2d70107792e8176e357486532c74234688
Poly <1014543299@qq.com>
2024-10-12 20:43:53 +0800

    进度更新_第一章完结

--- CPP/OpenGLExcercise01/main.cpp
+++ CPP/OpenGLExcercise01/main.cpp
@@ -13,22 +13,20 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+//#include <sstream>std::stringstream ss;
+#include "Camera.h"
 
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-		//glfwWindowShouldClose(window);
-	}
-}
 
-float vertices[] = {
-    // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-};
+
+
+
+//float vertices[] = {
+//    // positions          // colors           // texture coords
+//     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+//     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+//    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+//    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+//};
  
 //float vertices[] = {
 //	// positions         // colors
@@ -38,6 +36,49 @@ float vertices[] = {
 //
 //};
 
+float vertices[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
 
 unsigned int indices[] = {
 	// 注意索引从0开始! 
@@ -55,27 +96,66 @@ unsigned int indices[] = {
 //	0, 1, 2,   // first triangle
 //};
 
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+};
 
-const char* vertexshaderSource =
-"#version 330 core                                        \n "
-"layout (location = 0) in vec3 aPos;                      \n "
-"layout (location = 1) in vec3 aColor;                    \n "
-"out vec4 vertexColor;                                    \n "
-"void main(){                                             \n "
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);         \n "
-//"vertexColor = vec4(1.0f,0.5f,0.2f,1.0);      \n "
-"vertexColor = vec4(aColor.x,aColor.y,aColor.z,1.0); }     \n ";
 
-const char* fragshaderSource =
-"#version 330 core                                 \n "
-"out vec4 FragColor;                               \n "
-"in vec4 vertexColor;                              \n "
-"void main(){                                      \n "
-"    FragColor = vertexColor;}                     \n ";
+//const char* vertexshaderSource =
+//"#version 330 core                                        \n "
+//"layout (location = 0) in vec3 aPos;                      \n "
+//"layout (location = 1) in vec3 aColor;                    \n "
+//"out vec4 vertexColor;                                    \n "
+//"void main(){                                             \n "
+//"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);         \n "
+////"vertexColor = vec4(1.0f,0.5f,0.2f,1.0);      \n "
+//"vertexColor = vec4(aColor.x,aColor.y,aColor.z,1.0); }     \n ";
+//
+//const char* fragshaderSource =
+//"#version 330 core                                 \n "
+//"out vec4 FragColor;                               \n "
+//"in vec4 vertexColor;                              \n "
+//"void main(){                                      \n "
+//"    FragColor = vertexColor;}                     \n ";
 
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+////camera
+//glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+//glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+//glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+//
+//// settings
+//const unsigned int SCR_WIDTH = 800;
+//const unsigned int SCR_HEIGHT = 600;
+//
+//float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+//float pitch = 0.0f;
+//float fov = 45.0f;
+
+bool firstMouse = true;
+float lastX = 4000 / 2.0;
+float lastY = 2000 / 2.0;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f), glm::radians(-15.0f), glm::radians(180.0f), glm::vec3(0, 1.0f, 0));
 
 int main(){
-
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -85,7 +165,7 @@ int main(){
 	//Open GLFW Window
 	 //如果此处声明直接用GLFWwindow window的话 可能没办法用window的指针(point)去做事情，所以用GLFWwindow* window
 	//glfwCreateWindow()本身是返回一个Point指针
-	GLFWwindow* window = glfwCreateWindow(800, 600, "My OpenGL Game", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "My OpenGL Game", NULL, NULL);
 	if (window == NULL)
 	{
 		printf("Open Window False");
@@ -94,6 +174,8 @@ int main(){
 	}
 	//MakeContex，意思接下来使用该window
 	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
 
 	//Init GLEW
 	glewExperimental = true;
@@ -105,12 +187,13 @@ int main(){
 	}
 
 	//开窗！
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, 1920, 1080);
 	
 	Shader* myshader = new Shader("vertexSource.txt" ,"fragmentSource.txt");
 
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
 	//VBO：Vertex Buffer Objects，缓存CPU传入的模型信息（一长串String）；
 	//VAO：VertexArraysObject ，解析VBO中的信息，一般来说一个模型，就用一个VAO来缓存它
 	//有了VAO后，模型顶点，UV，法线信息明了了，我么就可以方便单独选择顶点为传入VertexShader的数据
@@ -123,43 +206,50 @@ int main(){
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);//给VBO配置ID
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);//绑定VBO：将该VBO连接到VAO的ArrayBuffer接口
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//unsigned int instanceVBO;
+	//glGenBuffers(1, &instanceVBO);
+	//glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * 10, &cubePositions[0], GL_STATIC_DRAW);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	////编译着色器代码
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexshaderSource, NULL);
-	glCompileShader(vertexShader);
+	//unsigned int vertexShader;
+	//vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//glShaderSource(vertexShader, 1, &vertexshaderSource, NULL);
+	//glCompileShader(vertexShader);
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragshaderSource, NULL);
-	glCompileShader(fragmentShader);
+	//unsigned int fragmentShader;
+	//fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//glShaderSource(fragmentShader, 1, &fragshaderSource, NULL);
+	//glCompileShader(fragmentShader);
 
 	//将着色器代码链接到着色器程序（Shader Program Object）
 	//为了能：激活对象的时候激活着色器程序，然后被调用，即控制该shader什么时候被调用！
-	unsigned int shaderprogram;
-	shaderprogram = glCreateProgram();
-	glAttachShader(shaderprogram, vertexShader);
-	glAttachShader(shaderprogram, fragmentShader);
-	glLinkProgram(shaderprogram);
+	//unsigned int shaderprogram;
+	//shaderprogram = glCreateProgram();
+	//glAttachShader(shaderprogram, vertexShader);
+	//glAttachShader(shaderprogram, fragmentShader);
+	//glLinkProgram(shaderprogram);
 
 	//配置数据挖取（VBO=>VAO.ARRAY_BUFFER）的规则
 	//类比Unity中的TEXCOORD0 TEXCOORD1 TEXCOORD2
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE ,8 * sizeof(float), (void*)0);//分配index0,挖3个值，每隔6个数挖一次，起始点偏移0个值
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE ,5 * sizeof(float), (void*)0);//分配index0,挖3个值，每隔5个数挖一次，起始点偏移0个值
 	glEnableVertexAttribArray(0);//激活
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));	//分配index1,挖3个值，每隔6个数挖一次，起始点偏移3个值
-	glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));	//分配index1,挖3个值，每隔6个数挖一次，起始点偏移3个值
+	//glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));	//分配index1,挖2个值，每隔6个数挖一次，起始点偏移6个值
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));	//分配index1,挖2个值，每隔5个数挖一次，起始点偏移63个值
 	glEnableVertexAttribArray(2);
 
 	//生成一张默认贴图，Bind到VAO的接口上，准备采样
@@ -208,11 +298,17 @@ int main(){
 
 	stbi_image_free(data2);
 
+	//创建相机
+	//Camera camera(glm::vec3(0, -1.0f, 5.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1.0f, 0));
+
+
 	glm::mat4 trans = glm::mat4(1.0f);
 	glm::mat4 modeMatrix = glm::mat4(1.0f);
-	modeMatrix = glm::rotate(modeMatrix , glm::radians( -55.0f ), glm::vec3(1.0f, 0, 0));
+	modeMatrix = glm::rotate(modeMatrix , glm::radians( -0.0f ), glm::vec3(1.0f, 1.0f, 0));
 	glm::mat4 viewMatrix = glm::mat4(1.0f);
-	viewMatrix = glm::translate(viewMatrix, glm::vec3(0, 0,-3.0f));
+	//viewMatrix = glm::translate(viewMatrix, glm::vec3(0, 0,-3.0f));
+	//viewMatrix = camera.GetViewMatrix();
+
 	glm::mat4 projMatrix = glm::mat4(1.0f);
 	//相机张角值，宽高比，裁切平面
 	projMatrix = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
@@ -229,27 +325,56 @@ int main(){
 		// 设置底色
 		//也用于测试之前的配置是否成功
 		glClearColor(0.3f, 0.3f, 0.5f, 1.0f);//先配置好我要将屏幕清成生么颜色
-		glClear(GL_COLOR_BUFFER_BIT); //执行清理(这里只执行ColorBuffer的清理)，此外还有GL_DEPTH_BUFFER_BIT，GL_DEPTH_STENCIL_BIT(深度，模板)
+		//执行清理(这里只执行ColorBuffer的清理)，此外还有GL_DEPTH_BUFFER_BIT，GL_DEPTH_STENCIL_BIT(深度，模板)
+		//清理上一帧的Z缓冲
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, TexBufferA);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, TexBufferB); 
+
+		//VAO：用于使用glDrawArrays的参数 Vertices Array?
 		glBindVertexArray(VAO);
+		//EBO：用于使用glDrawElements的参数
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
-		glUseProgram(myshader->ID);
-		myshader->use();
-		glUniform1i(glGetUniformLocation(myshader->ID, "ourTexture"), 0);
-		glUniform1i(glGetUniformLocation(myshader->ID, "ourTexture2"), 1);
-		//将transformLoc传入Vert中的 "transform"
-		unsigned int transformLoc = glGetUniformLocation(myshader->ID, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-		glUniformMatrix4fv(glGetUniformLocation(myshader->ID, "modeMatrix"), 1, GL_FALSE, glm::value_ptr(modeMatrix));
-		glUniformMatrix4fv(glGetUniformLocation(myshader->ID, "viewMatrix"), 1, GL_FALSE, glm::value_ptr(viewMatrix));
-		glUniformMatrix4fv(glGetUniformLocation(myshader->ID, "projMatrix"), 1, GL_FALSE, glm::value_ptr(projMatrix));
+		viewMatrix = camera.GetViewMatrix();
 
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (int i = 0; i < 10; i++)
+		{
+			glm::mat4 modeMatrix2 = glm::mat4(1.0f);//初始化
+			modeMatrix2 = glm::translate(modeMatrix2, cubePositions[i]);
+
+			glUseProgram(myshader->ID);
+			myshader->use();
+			glUniform1i(glGetUniformLocation(myshader->ID, "ourTexture"), 0);//将"ourTexture"传入GL_TEXTURE0
+			glUniform1i(glGetUniformLocation(myshader->ID, "ourTexture2"), 1);//将"ourTexture"传入GL_TEXTURE1
+			//将transformLoc传入Vert中的 "transform"
+			unsigned int transformLoc = glGetUniformLocation(myshader->ID, "transform");
+			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+			
+			//for (unsigned int i = 0; i < 10; i++)
+			//{
+			//	stringstream ss ;
+			//	string index;
+			//	ss << i;
+			//	index = ss.str();
+			//	myshader.setVec3(("offsets[" + index + "]").c_str(), cubePositions[i]);
+			//	unsigned int offsetLoc = glGetUniformLocation(myshader->ID, "offset");
+			//	glUniform3fv(offsetLoc,1, 1.0f);
+			//}
+
+			glUniformMatrix4fv(glGetUniformLocation(myshader->ID, "modeMatrix"), 1, GL_FALSE, glm::value_ptr(modeMatrix2));
+			glUniformMatrix4fv(glGetUniformLocation(myshader->ID, "viewMatrix"), 1, GL_FALSE, glm::value_ptr(viewMatrix));
+			glUniformMatrix4fv(glGetUniformLocation(myshader->ID, "projMatrix"), 1, GL_FALSE, glm::value_ptr(projMatrix));
+
+			//每调用一次Draw函数，就是一次draw call！
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+			//glBindVertexArray(VAO);
+			//glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100);
+			//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		}
 
 		//check and call event swap the buffer
 		//Double buffer：如果只指示程序在屏幕绘制一个Buffer，由于计算该buffer需要时间，，再加上绘制期间很容易很杂讯干扰，所以Buffer绘制的间隔期间会有闪烁。
@@ -257,6 +382,7 @@ int main(){
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();  //获取键盘跟鼠标的输入
+		camera.UpdateCameraPos();
 	}
 
 	//点击关闭窗口时执行
@@ -265,3 +391,51 @@ int main(){
 	return 0;
 }
 
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+		//glfwWindowShouldClose(window);
+	}
+	float cameraSpeed = 0.05f; // adjust accordingly
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+		//cameraPos += cameraSpeed * cameraFront;
+		camera.speedZ = 1.0f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+		//cameraPos -= cameraSpeed * cameraFront;
+		camera.speedZ = -1.0f;
+	}
+	else {
+		camera.speedZ = 0.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		camera.speedR = -1.0f;
+		//cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		//cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		camera.speedR = 1.0f;
+	}
+	else{
+		camera.speedR = 0.0f;
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double xPos, double yPos) {
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+
+	float xoffset = xPos - lastX;
+	float yoffset = lastY - yPos;
+	lastX = xPos;
+	lastY = yPos;
+	//printf(" % f \n", xoffset);
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+
+}
